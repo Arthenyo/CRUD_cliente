58ea3b99715a86231a5198988c1bffdd901e2952
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-05-16 16:48:56 -0300

    PUT de cient

--- src/main/java/com/arthenyo/cliente/services/ClientService.java
+++ src/main/java/com/arthenyo/cliente/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
@@ -14,28 +15,37 @@ public class ClientService {
     @Autowired
     private ClientRepositorys clientRepositorys;
 
-
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAll(Pageable pageable){
         Page<Client> clients = clientRepositorys.findAll(pageable);
         return clients.map(x -> new ClientDTO(x));
     }
-
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Client client = clientRepositorys.findById(id).get();
         return new ClientDTO(client);
     }
-
+    @Transactional
     public ClientDTO insert(ClientDTO dto){
         Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = clientRepositorys.save(entity);
+        return new ClientDTO(entity);
+    }
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        Client entity = clientRepositorys.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = clientRepositorys.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
         entity.setIncome(dto.getIncome());
         entity.setBirthDate(dto.getBirthDate());
         entity.setChildren(dto.getChildren());
-
-        entity = clientRepositorys.save(entity);
-
-        return new ClientDTO(entity);
     }
 
 }
