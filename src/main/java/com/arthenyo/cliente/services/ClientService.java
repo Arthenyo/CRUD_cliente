adf71b5366172f06ece9ef78c0353bbe9a26c7f8
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-05-16 16:20:25 -0300

    POST de cient

--- src/main/java/com/arthenyo/cliente/services/ClientService.java
+++ src/main/java/com/arthenyo/cliente/services/ClientService.java
@@ -25,4 +25,17 @@ public class ClientService {
         return new ClientDTO(client);
     }
 
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = clientRepositorys.save(entity);
+
+        return new ClientDTO(entity);
+    }
+
 }
